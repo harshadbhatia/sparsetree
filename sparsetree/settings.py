6c3a3c848683601d61681d389131010988006e43
harshadbhatia <harshadbhatia2012@gmail.com>
2017-10-25 22:57:01 +1100

    Added some colors and life to the template, backed the old template as index_old.html

--- sparsetree/settings.py
+++ sparsetree/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0)p&il+afx--t+%8ok=yh&@(+=d3zylty4fq)b1ng+@kpqt@7t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['harshadbhatia.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'harshadbhatia.pythonanywhere.com']
 
 
 # Application definition
